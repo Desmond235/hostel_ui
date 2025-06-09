0a24528eceaf142e6d1bec6ae8b5ba831fafa46a
Desmond <adabedesmond@gmail.com>
2025-06-09 15:46:23 -0700

    Refactor BottomNavBar to include visibility control and improve icon display logic

--- lib/components/bottoom_nav_bar.dart
+++ lib/components/bottoom_nav_bar.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hostel_ui/data/data.dart';
 
 class BottomNavBar extends StatefulWidget {
-  const BottomNavBar({super.key});
+  const BottomNavBar({super.key, required this.isVisible});
+  final bool isVisible;
 
   @override
   State<BottomNavBar> createState() => _BottomNavBarState();
@@ -16,46 +17,50 @@ class _BottomNavBarState extends State<BottomNavBar> {
       bottom: 30,
       left: MediaQuery.of(context).size.width * 0.085,
       child: Center(
-        child: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(5),
-          decoration: BoxDecoration(
-            color: Color(0xffe0e0e0),
-            borderRadius: BorderRadius.circular(40),
-          ),
-          child: Row(
-            children: [
-              ...List.generate(Data.iconSet.length, (index) {
-                final isSelected = index == selectedIndex;
-                return Padding(
-                  padding: const EdgeInsets.all(3),
-                  child: InkWell(
-                    onTap: () {
-                      selectedIndex = index;
-                      setState(() {});
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        color: isSelected ? Color(0xff0ba357) : Colors.white,
-                        shape: BoxShape.circle,
+        child: AnimatedOpacity(
+          opacity: widget.isVisible ? 1.0 : 0.0,
+          duration: const Duration(milliseconds: 400),
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Color(0xffe0e0e0),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Row(
+              children: [
+                ...List.generate(Data.iconSet.length, (index) {
+                  final isSelected = index == selectedIndex;
+                  return Padding(
+                    padding: const EdgeInsets.all(3),
+                    child: InkWell(
+                      onTap: () {
+                        selectedIndex = index;
+                        setState(() {});
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          color: isSelected ? Color(0xff0ba357) : Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: isSelected
+                            ? Icon(
+                                Data.selectedIconSet[index],
+                                size: 35,
+                                color: Colors.white,
+                              )
+                            : Icon(
+                                Data.iconSet[index],
+                                size: 35,
+                                color: Colors.grey.shade700,
+                              ),
                       ),
-                      child: isSelected
-                          ? Icon(
-                              Data.selectedIconSet[index],
-                              size: 35,
-                              color: Colors.white,
-                            )
-                          : Icon(
-                              Data.iconSet[index],
-                              size: 35,
-                              color: Colors.grey.shade700,
-                            ),
                     ),
-                  ),
-                );
-              }),
-            ],
+                  );
+                }),
+              ],
+            ),
           ),
         ),
       ),
