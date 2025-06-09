3f2a673ba12bba82405ce1eeed51706334aac080
Desmond <adabedesmond@gmail.com>
2025-06-09 15:46:54 -0700

    Add propertyList and bottomList to Data class for enhanced property display

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -15,4 +15,50 @@ class Data{
     Icons.chat_bubble,
     Icons.person,
   ];
-}
+
+  static const List<Map<String, dynamic>> propertyList = [
+    {
+      "image": 'assets/images/house1.jpg',
+      "tag": "Rent",
+      "icon": Icons.favorite,
+      "price": "\$700",
+      "description": "Casa Solana"
+      },
+      {
+      "image": 'assets/images/house2.jpg',
+      "tag": "Apartment",
+      "icon": Icons.favorite,
+      "price": "\$239,340",
+      "description": "Serenity Hills"
+      },
+      {
+      "image": 'assets/images/house3.jpg',
+      "tag": "Apartment",
+      "icon": Icons.favorite,
+      "price": "\$450,100",
+      "description": "Beverely Hills"
+      },
+      {
+      "image": 'assets/images/house4.jpg',
+      "tag": "Rent",
+      "icon": Icons.favorite,
+      "price": "\$960",
+      "description": "Shai Hills"
+      }
+  ];
+
+  static final List<Map<String, dynamic>> bottomList = [
+    {
+      "icon": Image.asset('assets/images/bed.png'),
+      "description": '06',
+    },
+    {
+      "icon": Image.asset('assets/images/bathtub.png'),
+      "description": '04'
+    },
+    {
+      "icon": Image.asset('assets/images/location.png'),
+      "description": "Miame, FL"
+    }
+  ];
+  }
