f50ee3c6ddf5487e516203134f0dc9c94877f321
Desmond <adabedesmond@gmail.com>
2025-06-08 20:47:57 -0700

    Add Data class with icon sets for consistent icon usage

---
+++ lib/data/data.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Data{
+  static const List<IconData> iconSet = [
+    Icons.search,
+    Icons.favorite_outline,
+    Icons.home_outlined, 
+    Icons.chat_bubble_outline,
+    Icons.person_outline, 
+  ];
+  static const List<IconData> selectedIconSet = [
+    Icons.search,
+    Icons.favorite,
+    Icons.home,
+    Icons.chat_bubble,
+    Icons.person,
+  ];
+}
