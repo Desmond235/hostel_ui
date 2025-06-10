7730ebb7f9bfe5389b78a33c44e74169dc651034
Desmond <adabedesmond@gmail.com>
2025-06-09 20:03:33 -0700

    Refactor Mainscreen to integrate buildTitleContainer and remove redundant title container code

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:hostel_ui/components/bottoom_nav_bar.dart';
 import 'package:hostel_ui/components/build_card.dart';
+import 'package:hostel_ui/components/build_title_container.dart';
 import 'package:hostel_ui/components/chip.dart';
 
 class Mainscreen extends StatefulWidget {
@@ -57,31 +58,7 @@ class _MainscreenState extends State<Mainscreen> {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            bottom: size.height * 0.88,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(35),
-                  bottomLeft: Radius.circular(35),
-                ),
-              ),
-              // height: size.height * 0.14 ,
-              width: size.width,
-              child: Center(
-                child: Container(
-                  margin: const EdgeInsets.only(top: 40),
-                  child: Text(
-                    "Saved Property",
-                    style: TextTheme.of(context).titleSmall,
-                  ),
-                ),
-              ),
-            ),
-          ),
+          buildTitleContainer(size, context),
           Positioned(
             top: MediaQuery.of(context).size.height * 0.132,
             left: 0,
