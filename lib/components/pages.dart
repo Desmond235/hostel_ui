27760820459ab37791cf717f3e4b931424920276
Desmond <adabedesmond@gmail.com>
2025-06-11 14:42:21 -0700

    Add pages list to manage navigation between Mainscreen and ChatScreen

---
+++ lib/components/pages.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:hostel_ui/screens/chatscreen.dart';
+import 'package:hostel_ui/screens/mainscreen.dart';
+
+List<Widget>pages = [
+  Mainscreen(),
+  ChatScreen()
+];
