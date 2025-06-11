0481d71a551bbb571ef6c9444835e9e0a064043b
Desmond <adabedesmond@gmail.com>
2025-06-11 15:25:47 -0700

    Comment out import statement for ChatScreen in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hostel_ui/screens/chatscreen.dart';
+// import 'package:hostel_ui/screens/chatscreen.dart';
 import 'package:hostel_ui/screens/mainscreen.dart';
 
 void main() {
