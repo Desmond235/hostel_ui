8a350913854e4218c6cc0972f7ceb2b2601e3d75
Desmond <adabedesmond@gmail.com>
2025-06-13 09:57:25 -0700

    Update main.dart to set MainWrapper as the home widget instead of Mainscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hostel_ui/screens/main_wrapper.dart';
 // import 'package:hostel_ui/screens/chatscreen.dart';
 import 'package:hostel_ui/screens/mainscreen.dart';
 
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
         ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Mainscreen(),
+      home: const MainWrapper(),
       debugShowCheckedModeBanner: false,
     );
   }
