c7d742f0b0b186371d8e9fd60a5ea710f9f4af7e
Desmond <adabedesmond@gmail.com>
2025-06-13 09:57:36 -0700

    Refactor pages function to include PlaceholderScreen widgets and pass ScrollController to Mainscreen

--- lib/components/pages.dart
+++ lib/components/pages.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hostel_ui/screens/chatscreen.dart';
 import 'package:hostel_ui/screens/mainscreen.dart';
-
+import 'package:hostel_ui/screens/placeholder_screen.dart';
+List<Widget> pages(ScrollController controller){
 List<Widget>pages = [
-  Mainscreen(),
-  ChatScreen()
+  PlaceholderScreen(title: 'Search'),
+  Mainscreen(scrollController: controller ,),
+  PlaceholderScreen(title: 'Home'),
+  ChatScreen(),
+  PlaceholderScreen(title: 'Account')
 ];
+ return pages;
+}
