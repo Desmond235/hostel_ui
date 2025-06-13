8b1a9feb30f74380568c8aea589578ac3ebcb467
Desmond <adabedesmond@gmail.com>
2025-06-13 09:56:39 -0700

    Refactor Mainscreen to use passed ScrollController and remove unused scroll logic

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -1,57 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:hostel_ui/components/bottoom_nav_bar.dart';
 import 'package:hostel_ui/components/build_card.dart';
 import 'package:hostel_ui/components/build_title_container.dart';
 import 'package:hostel_ui/components/chip.dart';
 
 class Mainscreen extends StatefulWidget {
-  const Mainscreen({super.key});
+  const Mainscreen({super.key, required this.scrollController});
+  final ScrollController scrollController;
 
   @override
   State<Mainscreen> createState() => _MainscreenState();
 }
 
 class _MainscreenState extends State<Mainscreen> {
-  late ScrollController _scrollController;
-  bool _isVisible = true;
-
-  @override
-  void initState() {
-    _scrollController = ScrollController();
-    _scrollController.addListener((){
-      final direction = _scrollController.position.userScrollDirection;
-      final atBottom = _scrollController.offset >= _scrollController.position.maxScrollExtent;
-
-      if(atBottom){
-        // if(!_isVisible ){
-        //   setState(() {
-        //     _isVisible = true;
-        //   });
-        // }
-      }else {
-      if(direction == ScrollDirection.reverse ){
-        setState(() {
-          _isVisible = false;
-        });
-      }else if(direction == ScrollDirection.forward){
-        setState(() {
-          _isVisible = true;
-        });
-      }
-      }
-
-    });
-    super.initState();
-    
-  }
-
-   @override
-    void dispose() {
-      _scrollController.dispose();
-      super.dispose();
-    }
-
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -81,12 +41,11 @@ class _MainscreenState extends State<Mainscreen> {
                   ),
                   SizedBox(
                     height: size.height   * 0.85,
-                    child: BuildCard(controller: _scrollController,))
+                    child: BuildCard(controller: widget.scrollController,))
                 ],
               )
             ),
           ),
-          BottomNavBar(isVisible: _isVisible,),
         ],
       ),
     );
