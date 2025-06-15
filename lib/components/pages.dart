730d6965bdc6371c8fbdc12e317de73224df2a62
Desmond <adabedesmond@gmail.com>
2025-06-14 23:29:21 -0700

    Update pages function to accept ChatList ScrollController for improved chat screen integration

--- lib/components/pages.dart
+++ lib/components/pages.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:hostel_ui/screens/chatscreen.dart';
 import 'package:hostel_ui/screens/mainscreen.dart';
 import 'package:hostel_ui/screens/placeholder_screen.dart';
-List<Widget> pages(ScrollController controller){
+List<Widget> pages(ScrollController controller, ScrollController chatListController){
 List<Widget>pages = [
   PlaceholderScreen(title: 'Search'),
   Mainscreen(scrollController: controller ,),
   PlaceholderScreen(title: 'Home'),
-  ChatScreen(),
+  ChatScreen(controller: chatListController ,),
   PlaceholderScreen(title: 'Account')
 ];
  return pages;
