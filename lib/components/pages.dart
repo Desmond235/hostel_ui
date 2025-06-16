f48fb691dbe2166a9ece14c5937f3bae0706d682
Desmond <adabedesmond@gmail.com>
2025-06-16 01:31:04 -0700

    Replaced Mainscreen with FavoritesScreen in pages function

--- lib/components/pages.dart
+++ lib/components/pages.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hostel_ui/screens/chatscreen.dart';
-import 'package:hostel_ui/screens/mainscreen.dart';
+import 'package:hostel_ui/screens/favorites_screen.dart';
 import 'package:hostel_ui/screens/placeholder_screen.dart';
 List<Widget> pages(ScrollController controller, ScrollController chatListController){
 List<Widget>pages = [
   PlaceholderScreen(title: 'Search'),
-  Mainscreen(scrollController: controller ,),
+  FavoritesScreen(scrollController: controller ,),
   PlaceholderScreen(title: 'Home'),
   ChatScreen(controller: chatListController ,),
   PlaceholderScreen(title: 'Account')
